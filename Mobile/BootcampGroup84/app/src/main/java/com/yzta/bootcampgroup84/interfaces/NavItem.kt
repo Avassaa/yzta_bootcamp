524faed43229885b6f7d6359690e434a95903928
Avassaa <tutuncubaha@gmail.com>
2025-08-02 14:45:22 +0300

    feature: migrated to new ui from design & added firebase dependencies

--- Mobile/BootcampGroup84/app/src/main/java/com/yzta/bootcampgroup84/interfaces/NavItem.kt
+++
@@ -1,9 +0,0 @@
-package com.yzta.bootcampgroup84.interfaces
-
-import androidx.compose.ui.graphics.vector.ImageVector
-
-data class NavItem(
-    val label : String,
-    val icon : ImageVector,
-    val badgeCount : Int,
-)
--- Mobile/BootcampGroup84/app/src/main/java/com/yzta/bootcampgroup84/ui/screens/register/RegisterScreen.kt
+++
@@ -1,2 +0,0 @@
-package com.yzta.bootcampgroup84.ui.screens.register
-
